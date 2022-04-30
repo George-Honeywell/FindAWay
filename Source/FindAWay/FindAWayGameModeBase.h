911654c34160a19fa83f20545b73e19c05a3b799
GHoneywell <george.honeywell@live.co.uk>
2022-04-30 16:51:20 +0100

    Initial Project Commit.

---
+++ Source/FindAWay/FindAWayGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FindAWayGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FINDAWAY_API AFindAWayGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
